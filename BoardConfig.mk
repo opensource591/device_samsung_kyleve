69a738c207d917f253956924f1dcd9bd82c014d1
sumit <sumitrajdev5842@gmail.com>
2015-11-20 11:00:53 +0530

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,119 @@
+USE_CAMERA_STUB := false
+
+# inherit from the proprietary version
+-include vendor/samsung/kyleve/BoardConfigVendor.mk
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := hawaii
+TARGET_BOOTLOADER_BOARD_NAME := hawaii
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_VARIANT := cortex-a9
+#MULTIPLE DEVICE NAMES
+TARGET_OTA_ASSERT_DEVICE  := hawaii,kyleve,hawaii_ss_kyleve,kylevexx
+
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x82000000
+BOARD_KERNEL_PAGESIZE := 4096
+
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+# Setting this to avoid boot locks on the system from using the "misc" partition.
+BOARD_HAS_NO_MISC_PARTITION := true
+
+# Indicate that the board has an Internal SD Card
+#BOARD_HAS_SDCARD_INTERNAL := true
+
+
+# Allow to format EXT4 based systems
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_CACHE_DEVICE := /dev/block/mmcblk0p16
+BOARD_CACHE_FILESYSTEM := ext4
+BOARD_CACHE_FILESYSTEM_OPTIONS := rw
+BOARD_SYSTEM_DEVICE := /dev/block/mmcblk0p17
+BOARD_SYSTEM_FILESYSTEM := ext4
+BOARD_SYSTEM_FILESYSTEM_OPTIONS := rw
+BOARD_DATA_DEVICE := /dev/block/mmcblk0p19
+BOARD_DATA_FILESYSTEM := ext4
+BOARD_DATA_FILESYSTEM_OPTIONS := rw
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x800000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x800000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 943718400
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2638217216
+BOARD_FLASH_BLOCK_SIZE := 0x00020000
+
+TARGET_KERNEL_SOURCES := kernel/samsung/kyleve/kernel
+TARGET_KERNEL_CONFIG := bcm21664_hawaii_ss_kylevess_rev00_defconfig
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Hardware rendering
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/samsung/kyleve/config/egl.cfg
+BOARD_USE_MHEAP_SCREENSHOT := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+#Audio and Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+#BOARD_USES_GENERIC_AUDIO := true
+#OPENSOURCE_ALSA_AUDIO := true
+BOARD_USES_ALSA_AUDIO := true
+BRCM_ALSA_LIB_DIR=device/samsung/bcm_common/alsa-lib
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/kyleve/recovery.fstab
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+
+# Audio
+COMMON_GLOBAL_CFLAGS += -DMR0_AUDIO_BLOB -DSAMSUNG_BCM_AUDIO_BLOB
+# Audio
+BOARD_USE_SAMSUNG_SEPARATEDSTREAM := true
+BOARD_USES_LIBMEDIA_WITH_AUDIOPARAMETER := true
+
+# HWComposer
+BOARD_USES_HWCOMPOSER := true
+
+# OMX
+BOARD_HAVE_CODEC_SUPPORT := SAMSUNG_CODEC_SUPPORT
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CODEC_SUPPORT
+BOARD_NONBLOCK_MODE_PROCESS := true
+BOARD_USE_STOREMETADATA := true
+BOARD_USE_METADATABUFFERTYPE := true
+BOARD_USES_MFC_FPS := true
+BOARD_USE_S3D_SUPPORT := true
+BOARD_USE_CSC_FIMC := false
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+
+# Connectivity - Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WLAN_DEVICE_REV       := bcm4330
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_BAND                   := 802_11_ABG
+
+# Wi-Fi Tethering
+BOARD_HAVE_SAMSUNG_WIFI := true
+BOARD_LEGACY_NL80211_STA_EVENTS := true
+BOARD_NO_APSME_ATTR := true
+
+# RIL
+BOARD_RIL_CLASS := device/samsung/kyleve/ril/
